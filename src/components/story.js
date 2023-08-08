7b99beee63626d5aabdd176bc589a269a91d87f9
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-08-08 13:18:51 -0700

    Update story section, add placeholder image

--- src/components/story.js
+++ src/components/story.js
@@ -1,14 +1,29 @@
 import * as React from "react";
 import StoryText from "../../stories/index.mdx"
-import { title } from "./story.module.css"
+import { 
+    title,
+    body,
+    image, 
+    container 
+} from "./story.module.css"
+import { StaticImage } from "gatsby-plugin-image";
 
 const Story = () => {
 
     return (
-        <div>
+        <>
             <header className={title}>my story</header>
-            <StoryText />
-        </div>
+            <div className={container}>
+                <div className={body}>
+                    <StoryText />
+                </div>
+                <StaticImage 
+                    alt="A photo of me will go here"
+                    src="../images/placeholder_image.jpg"
+                    className={image}
+                />
+            </div>
+        </>
     )
 }
 
