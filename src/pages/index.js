de9cd6db135f79e9593c8faec0ea24d8130d8ce8
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-08-14 19:52:34 -0700

    Add contact info

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Project from '../components/project'
 import Seo from '../components/seo'
 import Writing from '../components/writing'
 import FunFact from '../components/funfact'
+import Contact from "../components/contact"
 
 const IndexPage = () => {
 
@@ -16,6 +17,7 @@ const IndexPage = () => {
       <Project />
       <Writing />
       <FunFact />
+      <Contact />
     </Layout>
   )
 }
