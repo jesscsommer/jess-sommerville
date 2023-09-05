a428500c33061e3bd4fa04905bffcc59cb1b276a
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-09-05 16:06:34 -0700

    Revert domain

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `Jess Sommerville`,
-    siteUrl: `https://www.jess-sommerville.com`,
+    siteUrl: `https://www.yourdomain.ltd`,
   },
   plugins: [
     "gatsby-plugin-image",
