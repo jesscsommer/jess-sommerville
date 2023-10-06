df5e1dc530f9287f137df4b38501665dd0904aa5
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-10-06 12:47:04 -0700

    Update title text

--- src/components/home.js
+++ src/components/home.js
@@ -11,7 +11,7 @@ const Home = () => {
     return (
         <div className={container}>
             <p className={myName}>Hi, I'm Jess Sommerville</p>
-            <header className={mainText}>full-stack developer</header>
+            <header className={mainText}>software developer</header>
             <header className={mainText}>& forever tinkerer</header>
             <p className={detailText}>I enjoy creating things from scratch,</p>
             <p className={detailText}>working between people and tech,</p>
